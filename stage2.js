df4b9b5719f830d1ca0f469baef42083173e33ad
apicard3141 <hello@adampicard.com>
2025-09-28 11:15:04 -0500

    Create stage2.js

---
+++ stage2.js
@@ -0,0 +1,2 @@
+console.log('[STAGE 2] Executed from GitHub Pages.');
+document.body.insertAdjacentHTML('beforeend', '<div style="position:fixed;top:0;left:0;background:#000;color:#fff;padding:5px;z-index:9999">✅ STAGE 2</div>');
